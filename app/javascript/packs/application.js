71216b9d244b4c2c137f7412ede7eefc6afd6c88
sebastien etienne <sebastienetienne.pro@gmail.com>
2020-05-04 12:26:49 +0200

    map on

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,11 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vic3dhbiIsImEiOiJjazlhY2ZxMG0wOGdqM25ueTcxazE3YnNhIn0.eCxqXHuJrfRcc5sQ82LUCQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
