7bd9532f538c367a71b084a00ca19c7fba8d38a2
sebastien etienne <sebastienetienne.pro@gmail.com>
2020-05-04 13:13:03 +0200

    marker on

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,20 @@ import "bootstrap";
 
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vic3dhbiIsImEiOiJjazlhY2ZxMG0wOGdqM25ueTcxazE3YnNhIn0.eCxqXHuJrfRcc5sQ82LUCQ';
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox()
+
+// document.addEventListener('turbolinks:load', () => {
+//   ;
+// })
+// il semble que ce bout de code soit le pb de chargement de la map (map blanche)
+
+
+
+// mapboxgl.accessToken = 'pk.eyJ1Ijoic2Vic3dhbiIsImEiOiJjazlhY2ZxMG0wOGdqM25ueTcxazE3YnNhIn0.eCxqXHuJrfRcc5sQ82LUCQ';
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11'
+// });
+
